9f0e0ba2c8b2f74e74613ab69dfbbb3cfd576426
Marc Mills <mdmills1990@gmail.com>
2019-02-27 18:27:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ class Neighborhood {
     return store.deliveries.filter(
       function(delivery){
         return delivery.neighborhoodId === this.id;
-      }.bind(this)
+      }
     );
   }
   
